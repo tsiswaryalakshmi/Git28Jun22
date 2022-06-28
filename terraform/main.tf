9b88bfd2257dc6b3efc0b020be220185a07ff0af
tsiswaryalakshmi <99864357+tsiswaryalakshmi@users.noreply.github.com>
2022-06-28 15:13:44 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "TerraformDemo"
     storage_account_name = "terraformcodeops"
     container_name       = "tfstatefile"
-   # key                  = "dev.terraform.tfstate"
+    key                  = "terraform.tfstate"
   }
 }
 module "RG" {
